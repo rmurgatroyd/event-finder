54cdf1b6876040a46c2461e8f6d870669d65e4b1
rmurgatroyd <48757964+rmurgatroyd@users.noreply.github.com>
2019-09-24 17:59:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Form from './components/Form';
 import Events from './components/Events';
 import Title from './components/Title';
 import axios from 'axios';
-// import Dropdown from './components/Dropdown';
 
 class App extends Component {
 constructor(props){
@@ -16,9 +15,6 @@ constructor(props){
     error: undefined,
   }
 }
-
-
-
 
   getEvents = async params => {
     const url = `https://www.eventbriteapi.com/v3/events/search/?location.address=${params.city}&categories=${params.category}&subcategories=${params.subcategory}&sort_by=date&token=${TOKEN HERE}`;
@@ -38,7 +34,6 @@ constructor(props){
                           error: "Please enter a valid location",
                         })
                       }
-
   };
 
   componentDidMount() {
@@ -71,10 +66,7 @@ console.log(subcategories)
 }
 };
 
-
 render(){
-
-
   return (
     <div className="App">
 <Title title="Event Finder" />
